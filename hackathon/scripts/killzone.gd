cbca90b90a7d944d8fcbf023fa3e21f2cc739a10
Hellopal <183968252+Hellopal007@users.noreply.github.com>
2025-08-03 11:02:37 +0530

    read description
    1. added two new scene home and retry
    2. two new scripts home.gd and retry.gd
    both scene are part of UI
    background and font can be changed easily by selecting pane node then add any image or texture you want

--- hackathon/scripts/killzone.gd
+++ hackathon/scripts/killzone.gd
@@ -22,4 +22,5 @@ func _on_body_entered(body: Node2D) -> void:
 		
 func _on_death_timer_timeout() -> void:
 	Engine.time_scale = 1.0 # reset time scale before reloading to avoid affecting the next scene load
-	get_tree().reload_current_scene() # Reload the current scene = restarting the level
+	get_tree().change_scene_to_file("res://scenes/retry.tscn") # Reload the current scene = restarting the level
+#hey i changed the above line from reload scene to change scene to file
